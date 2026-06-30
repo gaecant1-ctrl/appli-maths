260916ebc3cb4c24be9b3a9efbb68c2d22fecfa6
gaecant1-ctrl <gaecant1@gmail.com>
2026-06-30 10:38:38 +0200

    Delete equation directory

--- equation/types.js
+++
@@ -1,9 +0,0 @@
-import { registerArithmeticTypes } from "./typesArithmetic.js";
-import { registerAlgebraicTypes } from "./typesAlgebraic.js";
-import { registerEquationTypes } from "./typesEquation.js";
-
-export function registerAllTypes(registry) {
-  registerArithmeticTypes(registry);
-  registerAlgebraicTypes(registry);
-  registerEquationTypes(registry);
-}
